5306e09cfb5a671b8d80e2616a00fe8a0e1e1138
R2D2Alfa <arturo.ad2203@comunidad.unam.mx>
2021-05-29 19:16:05 -0500

    FEAT: inicio de flappy birds

---
+++ dynamics/js/FlappyBirds.js
@@ -0,0 +1,19 @@
+//let bird = document.getElementById("bird");
+window.document.addEventListener("load", ()=>{
+let tubo1 = document.getElementById("tubo1");
+let tubo2 = document.getElementById("tubo2");
+let tubo3 = document.getElementById("tubo3");
+
+
+
+
+
+tubo1.setAttribute("height", "200px")
+tubo2.setAttribute("height", "300px")
+tubo3.setAttribute("height", "400px")
+})
+
+
+
+
+
